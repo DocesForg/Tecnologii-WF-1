91e10dac69e0222f764e256577181df2d888b61b
Виталий Мальцев <71219669+DocesForg@users.noreply.github.com>
2025-05-20 21:44:32 +0300

    Add files via upload

---
+++ WF3/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WF3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(118, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 105);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "а =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "b =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(238, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "c =";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(42, 13);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(44, 20);
+            this.textBoxA.TabIndex = 4;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(143, 13);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(44, 20);
+            this.textBoxB.TabIndex = 5;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(266, 13);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(44, 20);
+            this.textBoxC.TabIndex = 6;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(366, 9);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalculate.TabIndex = 7;
+            this.buttonCalculate.Text = "Вычислить";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.ButtonCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 160);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Button buttonCalculate;
+    }
+}
+
---
+++ WF3/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WF3
+{
+    public partial class Form1 : Form
+    {
+        private const double ScaleX = 30; // Масштаб по оси X
+        private const double ScaleY = 20; // Масштаб по оси Y
+
+        // Поля для хранения данных графика
+        private double _a = double.NaN;
+        private double _b = double.NaN;
+        private double _c = double.NaN;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Подписка на события
+            
+            pictureBox1.Paint += pictureBox1_Paint;
+            Resize += Form1_Resize;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate(); // Перерисовка при изменении размера
+        }
+
+        private void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            if (!ParseInputs(out double a, out double b, out double c))
+                return;
+
+            // Сохранение значений для отрисовки
+            _a = a;
+            _b = b;
+            _c = c;
+
+            pictureBox1.Invalidate(); // Инициируем перерисовку
+            FindMinimum(a, b, c);
+        }
+
+        private bool ParseInputs(out double a, out double b, out double c)
+        {
+            a = b = c = 0;
+
+            if (!double.TryParse(textBoxA.Text, out a) ||
+                !double.TryParse(textBoxB.Text, out b) ||
+                !double.TryParse(textBoxC.Text, out c))
+            {
+                MessageBox.Show("Ошибка: Все значения должны быть числами");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void FindMinimum(double a, double b, double c)
+        {
+            double f(double x) => Math.Sin(x) + Math.Cos(2 * x);
+
+            double va = f(a), vb = f(b), vc = f(c);
+            double min = Math.Min(va, Math.Min(vb, vc));
+
+            string result = "";
+            if (va == min) result += $"a={a:F2}: {va:F4}\n";
+            if (vb == min) result += $"b={b:F2}: {vb:F4}\n";
+            if (vc == min) result += $"c={c:F2}: {vc:F4}\n";
+
+            MessageBox.Show($"Минимальное значение в точках:\n{result}");
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Не рисуем, если данные не заданы
+            if (double.IsNaN(_a) || double.IsNaN(_b) || double.IsNaN(_c))
+                return;
+
+            var g = e.Graphics;
+            var pictureBox = (PictureBox)sender;
+            int width = pictureBox.Width;
+            int height = pictureBox.Height;
+
+            // Оси координат
+            using (Pen axisPen = new Pen(Color.LightGray, 1))
+            {
+                g.DrawLine(axisPen, 0, height / 2, width, height / 2); // Ось X
+                g.DrawLine(axisPen, width / 2, 0, width / 2, height); // Ось Y
+            }
+
+            // График функции
+            using (Pen functionPen = new Pen(Color.Blue, 2))
+            {
+                Point previousPoint = new Point();
+                for (double x = -10; x <= 10; x += 0.1)
+                {
+                    double y = Math.Sin(x) + Math.Cos(2 * x);
+                    int px = (int)(width / 2 + x * ScaleX);
+                    int py = (int)(height / 2 - y * ScaleY);
+
+                    if (x == -10)
+                    {
+                        previousPoint = new Point(px, py);
+                        continue;
+                    }
+
+                    g.DrawLine(functionPen, previousPoint, new Point(px, py));
+                    previousPoint = new Point(px, py);
+
+                    // Отображение точек a, b, c
+                    if (Math.Abs(x - _a) < 0.05 ||
+                        Math.Abs(x - _b) < 0.05 ||
+                        Math.Abs(x - _c) < 0.05)
+                    {
+                        g.FillEllipse(Brushes.Red, px - 3, py - 3, 6, 6);
+                    }
+                }
+            }
+        }
+    }
+}
