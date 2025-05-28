471fa01c3207d3af060f40ab909b30275dceac6f
Виталий Мальцев <71219669+DocesForg@users.noreply.github.com>
2025-05-28 18:05:22 +0300

    Add files via upload

---
+++ WF4/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WF4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(117, 33);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSearch.TabIndex = 0;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(129, 67);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.Text = "Найти";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.Location = new System.Drawing.Point(12, 109);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(321, 134);
+            this.listBoxResults.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите число для поиска:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 255);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxResults);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listBoxResults;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WF4/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WF4
+{
+    public partial class Form1 : Form
+    {
+        private int[] array = new int[100];
+        private int[] originalArray;
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateRandomArray();
+            DisplayArray(originalArray, "Исходный массив:");
+        }
+
+        private void GenerateRandomArray()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 1000); // Числа от 1 до 999
+            }
+
+            originalArray = new int[array.Length];
+            Array.Copy(array, originalArray, array.Length);
+        }
+
+        private void DisplayArray(int[] arr, string title)
+        {
+            listBoxResults.Items.Add(title);
+
+            // Преобразуем массив в строку с пробелами
+            string arrayLine = string.Join(" ", arr);
+
+            // Разбиваем на строки по 100 символов
+            int maxLineLength = 50;
+            for (int i = 0; i < arrayLine.Length; i += maxLineLength)
+            {
+                int length = Math.Min(maxLineLength, arrayLine.Length - i);
+                listBoxResults.Items.Add(arrayLine.Substring(i, length));
+            }
+
+            listBoxResults.Items.Add(""); // Пустая строка между блоками
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(textBoxSearch.Text))
+                {
+                    MessageBox.Show("Введите значение для поиска!");
+                    return;
+                }
+
+                int searchValue = int.Parse(textBoxSearch.Text);
+
+                // Очищаем список перед новым выводом
+                listBoxResults.Items.Clear();
+
+                // Вывод оригинального массива
+                DisplayArray(originalArray, "Исходный массив:");
+
+                // Линейный поиск
+                int linearIterations = LinearSearch(searchValue);
+                listBoxResults.Items.Add($"Линейный поиск: значение {searchValue} найдено за {linearIterations} итераций.");
+
+                // Быстрая сортировка
+                QuickSort(array, 0, array.Length - 1);
+
+                // Вывод отсортированного массива
+                DisplayArray(array, "Отсортированный массив:");
+
+                // Бинарный поиск
+                int binaryIterations = BinarySearch(searchValue);
+                listBoxResults.Items.Add($"Бинарный поиск: значение {searchValue} найдено за {binaryIterations} итераций.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private int LinearSearch(int value)
+        {
+            int iterations = 0;
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                iterations++;
+                if (originalArray[i] == value)
+                {
+                    return iterations;
+                }
+            }
+            return iterations; // Элемент не найден
+        }
+
+        private int BinarySearch(int value)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            int iterations = 0;
+
+            while (left <= right)
+            {
+                iterations++;
+                int mid = left + (right - left) / 2;
+
+                if (array[mid] == value)
+                {
+                    return iterations;
+                }
+                else if (array[mid] < value)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return iterations; // Элемент не найден
+        }
+
+        private void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(arr, low, high);
+                QuickSort(arr, low, pivotIndex - 1);
+                QuickSort(arr, pivotIndex + 1, high);
+            }
+        }
+
+        private int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+                    Swap(ref arr[i], ref arr[j]);
+                }
+            }
+            Swap(ref arr[i + 1], ref arr[high]);
+            return i + 1;
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
